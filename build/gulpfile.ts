4bdd9f15a614e4962ef7dd8541b3d6a96ba4be43
Rob Erekson <roberekson@gmail.com>
2022-01-30 20:49:48 -0800

    build: :construction_worker: Process images as build step

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,13 +1,23 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 const ts = require('gulp-typescript');
 const { resolve } = require('path');
 
 const paths = {
   html: resolve(__dirname, '../src/html'),
+  images: resolve(__dirname, '../src/images'),
   scss: resolve(__dirname, '../src/scss'),
   public: resolve(__dirname, '../public'),
 }
+
+const buildImages = () => (
+  gulp.src(`${paths.images}/**/*`)
+    .pipe(imagemin([
+      imagemin.mozjpeg({ quality: 80, progressive: true }),
+    ]))
+    .pipe(gulp.dest(`${paths.public}/images`))
+)
 
 const buildHtml = () => (
   gulp.src(`${paths.html}/**/*.html`)
@@ -20,9 +30,10 @@ const buildScss = () => (
     .pipe(gulp.dest(`${paths.public}/css`))
 );
 
-exports.build = gulp.series(buildHtml, buildScss);
+exports.build = gulp.series(buildHtml, buildScss, buildImages);
 
 exports.watch = () => {
+  gulp.watch(paths.images, buildImages);
   gulp.watch(paths.html, buildHtml);
   gulp.watch(paths.scss, buildScss);
 };
